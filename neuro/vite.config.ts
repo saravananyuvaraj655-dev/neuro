f7e9cb51943376504e318f6bec2412436e24cb4b
saravananyuvaraj655-dev <saravananyuvaraj655@gmail.com>
2026-04-19 23:49:54 +0530

    hgsgd

--- neuro/vite.config.ts
+++ neuro/vite.config.ts
@@ -3,10 +3,17 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: "/",   // ✅ IMPORTANT
+
   plugins: [
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+
+      workbox: {
+        navigateFallback: '/index.html'  // ✅ FIX BLANK SCREEN
+      },
+
       manifest: {
         name: 'NeuroTrack',
         short_name: 'NeuroTrack',
