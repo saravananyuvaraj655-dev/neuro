76c309e20a73723cc75df297ca757c40ea3b7291
saravananyuvaraj655-dev <saravananyuvaraj655@gmail.com>
2026-04-20 00:45:45 +0530

    dhdh

--- neuro/vite.config.ts
+++ neuro/vite.config.ts
@@ -1,8 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt'],
+
+      manifest: {
+        name: 'NeuroTrack AI',
+        short_name: 'NeuroTrack',
+        description: 'AI-Based Medical Assistant App',
+        theme_color: '#0f172a',
+        background_color: '#0f172a',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     dedupe: ['react', 'react-dom']
   }
